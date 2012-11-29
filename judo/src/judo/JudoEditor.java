80f4f1e6c7499cd3fb00ae1ad2950b37a5be806f
Cesar D Velez <velezcd@gmail.com>
2012-11-29 17:17:22 -0500

    added links in comments to presenation slides

--- judo/src/judo/JudoEditor.java
+++ judo/src/judo/JudoEditor.java
@@ -1,10 +1,15 @@
+/*
+Judo - A Java Markdown Editor = Cesar Velez | Fall 2012
+
+Uses icon set from famfamfam.com and markdownj from Peter Bevin.
+
+Mid-presentation slides: https://docs.google.com/presentation/d/1X1sawPPT97sr-QHPg3dyLVtHaKQC0y5RnoCQ6JSDyaY/edit
+Final Presentation ppt: https://docs.google.com/presentation/d/1a1p5BvJ3PKhax2piq9YcpWyX-PJkXFK-kS8JulEi7Fg/edit
+
+*/ 
+
 package judo;
-
 import java.util.ArrayList;
-
-
-// Uses icon set from famfamfam.com and markdownj from Peter Bevin.
-// PresentTION: https://docs.google.com/presentation/d/1a1p5BvJ3PKhax2piq9YcpWyX-PJkXFK-kS8JulEi7Fg/edit
 public class JudoEditor
 {
 
